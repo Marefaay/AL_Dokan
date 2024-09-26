f606b4e18883d7bbefef105bc2634c0da7f72f9a
Marefaay <mrefaay271@gmail.com>
2024-09-26 21:15:09 +0300

    Describe your changes

--- src/Components/StoreMainSection.js
+++ src/Components/StoreMainSection.js
@@ -7,7 +7,12 @@ const StoreMainSection = () => {
       <span>
         <Link to="/">Home</Link>/store
       </span>
-      <svg id="shape" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <svg
+        id="shape"
+        xmlns="http://www.w3.org/2000/svg"
+        className="d-sm-none d-md-none d-lg-block"
+        viewBox="0 0 1440 320"
+      >
         <path
           fill="#0d6efd"
           fill-opacity="1"
